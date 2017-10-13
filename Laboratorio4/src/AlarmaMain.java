1e816d520ccefffe687c94a4f40ba7e7b42c321d
David <David@LAPTOP-4MAV6DKP>
2017-10-13 08:54:17 -0600

    Se creo una nueva clase llamada AlarmaEmergencia para realizar el ejercicio 3 la cual se extiende de la clase Alarma. Ademas se modifico la clase AlarmaMain para poder realizar el ejercicio 3 utilizando la clase AlarmaEmergencia

--- Laboratorio4/src/AlarmaMain.java
+++ Laboratorio4/src/AlarmaMain.java
@@ -3,10 +3,10 @@
 
 public class AlarmaMain {
 	public static void main(String[] args) {
-		Alarma alarmaIncendio = new Alarma();
-                System.out.println(alarmaIncendio);   // emergencia Ring…Ring…Ring…Ring
+		AlarmaEmergencia alarmaIncendio = new AlarmaEmergencia();
 		alarmaIncendio.m1();                  // emergencia 1
 		alarmaIncendio.m2();                  // alarma 1 / timbre 1
+                System.out.println(alarmaIncendio);   // emergencia Ring…Ring…Ring…Ring
 		
 	}
 }
