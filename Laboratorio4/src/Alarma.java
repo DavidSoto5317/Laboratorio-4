d7108d773c623b9c3fa0dc39df41718ea07815d5
David <David@LAPTOP-4MAV6DKP>
2017-10-13 08:43:38 -0600

    Se modifico la clase Alarma de modo que se pudiera realizar el Ejercicio 2, agregando los metodos borrados anteriormente

--- Laboratorio4/src/Alarma.java
+++ Laboratorio4/src/Alarma.java
@@ -3,12 +3,12 @@ public class Alarma extends Timbre {
 		System.out.println("alarma 1");
 	}
         
-//	 
-//	public void m2() {
-//		super.m1();
-//	}
-//	 
-//	public String toString() {
-//		return super.toString() + "…" + super.toString();
-//	}
+	 
+	public void m2() {
+		super.m1();
+	}
+	 
+	public String toString() {
+		return super.toString() + "…" + super.toString();
+	}
 }
